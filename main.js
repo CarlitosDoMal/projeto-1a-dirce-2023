0573bb81ce3d33481e43b350a1cd1c277603a86e
CarlitosDoMal <131264361+CarlitosDoMal@users.noreply.github.com>
2024-09-27 09:00:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const caixaprincipal=document.queryselector(.caixaprincipal"):
+const caixaperguntas=document.queryselector(.caixas-perguntas"):
+const caixaAlternativas=document.queryselector(.caixa-alternativas"):
+const caixareultado= document.queryselector(".caixa resultado"):
+const textoResultado= document.queryselector(".texto-resultado"):
+
+const perguntas = []
+     [
+        enunciado:"Assim que saiu da escola voce se depara com uma nova tecnologia,um chat que consegue reponder todas as dúvidas que uma p
+        alternativas"; [
+            "isso é asustador!",
+            "isso é maravilhoso!"
+{SS}
+        ]
+     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
